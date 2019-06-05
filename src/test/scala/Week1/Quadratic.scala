63273a8a6aadb1931e5f16b5a510e0377f0e8967
prayas2409 <prayas.hemraj.pagade@gmail.com>
2019-06-05 19:18:39 +0530

    Gambler game in Scala

--- src/test/scala/Week1/Quadratic.scala
+++ src/test/scala/Week1/Quadratic.scala
@@ -9,38 +9,38 @@ class Quadratic {
       var sign: String = ""
       var rt1: Float = 0
       var rt2: Float = 0
-      System.out.println("The equation is " + a + "x^2+" + b + "x+" + c + "=0");
+      println("The equation is " + a + "x^2+" + b + "x+" + c + "=0")
 
-      delta = b * b - 4 * a * c;
+      delta = b * b - 4 * a * c
       //// printing delta
-      System.out.println("delta is " + delta);
-      srt = Math.sqrt(delta);
+      println("delta is " + delta)
+      srt = Math.sqrt(delta)
       //// if the squre root is imaginary
-      if ((Math.sqrt(delta).isNaN)) {
+      if (Math.sqrt(delta).isNaN) {
         //// if the sqrt is imaginary root
-        delta = -delta;
+        delta = -delta
         srt = scala.math.sqrt(delta)
         rt1 = (srt / 2 * a).toFloat
         rt2 = (-srt / 2 * a).toFloat
         //// to check the sign that has to be used as + - should be - else +
         if (rt2 > 0)
-          sign = "-";
+          sign = "-"
         else
-          sign = "";
+          sign = ""
 
-        System.out.printf("The roots of the equations are " + (-b / 2 * a) + "+" + rt1 + "i and" + (-b / 2 * a)
-          + sign + rt2 + "i");
+        println("The roots of the equations are " + (-b / 2 * a) + "+" + rt1 + "i and" + (-b / 2 * a)
+          + sign + rt2 + "i")
       } else {
         //// For real roots
-        srt = Math.sqrt(delta);
+        srt = Math.sqrt(delta)
         rt1 = ((srt - b) / 2 * a).toFloat
         rt2 = ((-srt - b) / 2 * a).toFloat
-        System.out.printf("The roots of the equations are " + rt1 + " and " + rt2);
+        println("The roots of the equations are " + rt1 + " and " + rt2)
       }
     }
     catch {
       case e :Exception =>
-        System.out.println("The process is stopped because " + e);
+        println("The process is stopped because " + e)
       }
     }
   }
