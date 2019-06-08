c03d89073711a4ae781e3e8f9f2013a0ea237590
prayas2409 <prayas.hemraj.pagade@gmail.com>
2019-06-08 19:13:58 +0530

    Multi column Dataframes using scala spark

--- src/test/scala/Week5/Dataframe_file.scala
+++ src/test/scala/Week5/Dataframe_file.scala
@@ -1,6 +1,7 @@
 package Week5
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Row, SparkSession, types}
+import org.apache.spark.sql.types.{DataType, IntegerType, StringType, StructField, StructType}
 
 object Dataframe_file {
 
@@ -12,30 +13,68 @@ object Dataframe_file {
       .master("local")
       .getOrCreate()
 
-    // Defining the structure of the data frame
+    val someData = Seq(
+      Row(24, "Prayas","Nagpur"),
+      Row(23, "Saurabh","Mumbai"),
+      Row(25, "Deepak","Jaipur")
+    )
 
-    //    var schema = StructType(
-    //      Array(StructField("Name",StringType,false),
-    //      StructField("Age",IntegerType,true),
-    //        StructField("City",StringType,true)))
+    val someSchema = List(
+      StructField("Age", IntegerType, true),
+      StructField("Name", StringType, true),
+      StructField("City",StringType)
+    )
 
+    val someDF = spark.createDataFrame(
+      spark.sparkContext.parallelize(someData),
+      StructType(someSchema)
+    )
 
-    //
+    someDF.show()
+
+    val schema = StructType(
+      Array(StructField("Age",IntegerType,true)
+      ,StructField("Name",StringType,true),
+      StructField("City",StringType,true)))
+
+    println("new data frame")
+    val list = List((23,"Prayas","ngp"), (24,"saurabh","jaipur"))
+    val scm = List(("Agea",IntegerType,true),("namea",StringType,false),("Citya",StringType,true))
+    var d = spark.sparkContext.parallelize(list)
+
+    // Importing the spark toDF
+    val sc = spark.sparkContext
+    val sqlContext= new org.apache.spark.sql.SQLContext(sc)
+    import sqlContext.implicits._
+
+    var df = d.toDF("Age","UserName","City")
+    df.show()
+    df.printSchema()
+
+    println("Third dataframe")
+    var dfnew = spark.createDataFrame(list)
+    dfnew.show()
+    dfnew.printSchema()
+
+    println("Fourth DF")
+    val values = spark.sparkContext.parallelize(List(List("1", 1) ,List("2", 2) ,List("3", 3),List("4",4)))
+    val rowRDD = values.map(e => Row(e(0), e(1)))
+    val schem = StructType(
+      Array(StructField("Name",StringType,true),
+        StructField("Age",IntegerType,true)))
+
+    val df2 = spark.createDataFrame(rowRDD,schem)
+    df2.show()
+    println(df2.printSchema())
+
     //    var df = spark.read
     //  .option("inferSchema", "true")
     //  .option("header", "false")
     //  .schema(schema)
     //  .csv("src/test/scala/Week5/data/a.txt")
-    //
+
     //    df.show()
     //    df.printSchema()
-
-    //    var list = List(1,2,23,34,5)
-
-    //    var schema = StructType(
-    //     StructField("Number",IntegerType,false)::Nil)
-
-    //    println("Printing file")
 
     /*
     var file = spark.sparkContext.textFile("src/test/scala/Week5/data/a.txt")
@@ -49,12 +88,18 @@ object Dataframe_file {
     rows.foreach(println(_))
     */
 
-    //    var list = spark.sparkContext.parallelize(List(List("prayas",23,"ngp"), List("sam",32,"kamp"),List("nik",54,"jbl")))
-    //    println("Printing the list"+list)
-    //    list.foreach(println(_))
-    //
-    //    var r = list.map(Row(_))
-    //    println("printing"+r)
+    println("Fifth Dataframe")
+    var newlist = spark.sparkContext.parallelize(List(List("prayas",23,"ngp"), List("sam",32,"kamp"),List("nik",54,"jbl")))
+    var rddlist = newlist.map(x => Row(x(0),x(1),x(2)))
+    var schema4 = StructType(Array(
+        StructField("Name",StringType,true),
+        StructField("Age",IntegerType,false),
+        StructField("City",StringType,true)
+      ))
+
+    var df4 = spark.createDataFrame(rddlist,schema4)
+    df4.show()
+    df.printSchema()
     //
     //    println("vals in r")
     //    r.foreach(println(_))
