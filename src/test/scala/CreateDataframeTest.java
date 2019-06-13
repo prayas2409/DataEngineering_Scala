6a09e80ea82eece49de0366227009a799322e9a8
prayas2409 <prayas.hemraj.pagade@gmail.com>
2019-06-13 16:05:47 +0530

    auto test cases

---
+++ src/test/scala/CreateDataframeTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CreateDataframeTest {
+
+    @Test
+    public void fromList() {
+
+    }
+
+    @Test
+    public void fromList1() {
+
+    }
+
+    @Test
+    public void fromFile() {
+    }
+
+    @Test
+    public void fromFile1() {
+    }
+}
